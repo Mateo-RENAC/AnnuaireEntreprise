08228a1a41c2cc18778aee3f11de15c638ce1a92
Mateo-RENAC <mateo.renac@gmail.com>
2024-10-14 13:33:17 +0200

    Problème avec les fichier xaml ManageServicesWindows impossible de l'ouvrir

--- Views/ManageServicesWindow.xaml.cs
+++ Views/ManageServicesWindow.xaml.cs
@@ -1,65 +1,27 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using AnnuaireEntreprise.Data;
-using AnnuaireEntreprise.Models;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-namespace AnnuaireEntreprise
+namespace AnnuaireEntreprise.Views
 {
+    /// <summary>
+    /// Logique d'interaction pour ManageServicesWindow.xaml
+    /// </summary>
     public partial class ManageServicesWindow : Window
     {
-        private AnnuaireContext _context;
-
         public ManageServicesWindow()
         {
             InitializeComponent();
-            _context = new AnnuaireContext();
-            LoadServices();
-        }
-
-        private void LoadServices()
-        {
-            ServicesListView.ItemsSource = _context.Services.ToList();
-        }
-
-        private void OnAddServiceButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(ServiceTextBox.Text))
-            {
-                var service = new Service { Nom = ServiceTextBox.Text };
-                _context.Services.Add(service);
-                _context.SaveChanges();
-                LoadServices();
-                ServiceTextBox.Clear();
-            }
-        }
-
-        private void OnEditServiceButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (ServicesListView.SelectedItem is Service selectedService && !string.IsNullOrEmpty(ServiceTextBox.Text))
-            {
-                selectedService.Nom = ServiceTextBox.Text;
-                _context.SaveChanges();
-                LoadServices();
-                ServiceTextBox.Clear();
-            }
-        }
-
-        private void OnDeleteServiceButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (ServicesListView.SelectedItem is Service selectedService)
-            {
-                _context.Services.Remove(selectedService);
-                _context.SaveChanges();
-                LoadServices();
-            }
-        }
-
-        private void OnServiceSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            if (ServicesListView.SelectedItem is Service selectedService)
-            {
-                ServiceTextBox.Text = selectedService.Nom;
-            }
         }
     }
 }
