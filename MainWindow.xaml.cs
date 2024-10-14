2031a36958f78dfc31a8b1cf6b04af82c021d71d
Mateo-RENAC <mateo.renac@gmail.com>
2024-10-14 11:09:58 +0200

    Initialisation and first integration of CRUD for site

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,74 @@
-﻿using System.Text;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AnnuaireEntreprise.Data;
+using AnnuaireEntreprise.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnnuaireEntreprise
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private AnnuaireContext _context;
+
         public MainWindow()
         {
             InitializeComponent();
+            _context = new AnnuaireContext();
+            LoadSites();
+            LoadServices();
+        }
+
+        private void LoadSites()
+        {
+            SiteComboBox.ItemsSource = _context.Sites.ToList();
+            SiteComboBox.DisplayMemberPath = "Ville";
+            SiteComboBox.SelectedValuePath = "Id";
+        }
+
+        private void LoadServices()
+        {
+            ServiceComboBox.ItemsSource = _context.Services.ToList();
+            ServiceComboBox.DisplayMemberPath = "Nom";
+            ServiceComboBox.SelectedValuePath = "Id";
+        }
+
+        private void OnSearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            var query = _context.Employes.Include(e => e.Service).Include(e => e.Site).AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchTextBox.Text))
+            {
+                query = query.Where(emp => emp.Nom.Contains(SearchTextBox.Text));
+            }
+
+            if (SiteComboBox.SelectedValue != null)
+            {
+                int siteId = (int)SiteComboBox.SelectedValue;
+                query = query.Where(emp => emp.SiteId == siteId);
+            }
+
+            if (ServiceComboBox.SelectedValue != null)
+            {
+                int serviceId = (int)ServiceComboBox.SelectedValue;
+                query = query.Where(emp => emp.ServiceId == serviceId);
+            }
+
+            EmployeesListView.ItemsSource = query.ToList();
+        }
+
+        private void EmployeesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EmployeesListView.SelectedItem is Employe selectedEmploye)
+            {
+                NomTextBlock.Text = selectedEmploye.Nom;
+                PrenomTextBlock.Text = selectedEmploye.Prenom;
+                TelephoneFixeTextBlock.Text = selectedEmploye.TelephoneFixe;
+                TelephonePortableTextBlock.Text = selectedEmploye.TelephonePortable;
+                EmailTextBlock.Text = selectedEmploye.Email;
+                ServiceTextBlock.Text = selectedEmploye.Service?.Nom;
+                SiteTextBlock.Text = selectedEmploye.Site?.Ville;
+            }
         }
     }
 }
